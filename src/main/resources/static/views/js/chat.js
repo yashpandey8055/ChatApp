092b26c9e24563576867a3978516a628566edc87
Yash Pandey <yashpandey8055@gmail.com>
2018-11-29 01:32:16 +0530

    Login page added

--- src/main/resources/static/views/js/chat.js
+++ src/main/resources/static/views/js/chat.js
@@ -1,29 +1,4 @@
-function HttpRequest(){
-	var obj = {};
-	
-	obj.get = function(url,params,callback){
-		
-		if(params!==null){
-			var requestParam = '' ;
-			params.forEach(function(value,key){
-				requestParam = requestParam+key+'='+value+'&'
-			});
-			url = url+'?'+requestParam;
-		}
-		
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.onreadystatechange = function(){
-			 if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-				 callback(this.responseText);
-			}
-		}
-		
-		xmlHttp.open("GET",url);
-		xmlHttp.setRequestHeader("Authorization","Bearer "+token);
-		xmlHttp.send(null);
-	}
-	return obj;
-}
+
 var turn = 180;
 function displayUserInformation(user){
 	//$(".flipper").css({"transform":"rotateY("+turn+"deg)"});
--- src/main/resources/static/views/js/config.js
+++ src/main/resources/static/views/js/config.js
@@ -1,1 +1,28 @@
 var env = "http://localhost:8080"
+	
+function HttpRequest(){
+	var obj = {};
+	
+	obj.get = function(url,params,callback){
+		
+		if(params!==null){
+			var requestParam = '' ;
+			params.forEach(function(value,key){
+				requestParam = requestParam+key+'='+value+'&'
+			});
+			url = url+'?'+requestParam;
+		}
+		
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function(){
+			 if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+				 callback(this.responseText);
+			}
+		}
+		
+		xmlHttp.open("GET",url);
+		xmlHttp.setRequestHeader("Authorization","Bearer "+token);
+		xmlHttp.send(null);
+	}
+	return obj;
+}
